c5549461fc0360195c158400a73d1bbabee3e969
JosephJohncross <josephibochi@gmail.com>
2023-10-25 04:34:27 +0100

    initial: project setup

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MagicVilla", Version = "v1" });
+});
+var app = builder.Build();
+
+// Configure the http request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "MagicVilla");
+    });
+}
+
+
+app.UseHttpsRedirection();
+app.Run();
